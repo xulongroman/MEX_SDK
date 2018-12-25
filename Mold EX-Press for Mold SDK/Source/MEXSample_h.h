bafd98bf48f6b698a9bcb1556c238ba27c724a9a
XU Long <LX3@dsone.3ds.com>
2018-12-25 23:36:02 +0800

    bugfix

--- Mold EX-Press for Mold SDK/Source/MEXSample_h.h
+++ Mold EX-Press for Mold SDK/Source/MEXSample_h.h
@@ -3,11 +3,11 @@
 /* this ALWAYS GENERATED file contains the definitions for the interfaces */
 
 
- /* File created by MIDL compiler version 8.00.0603 */
-/* at Tue Dec 25 15:31:06 2018
+ /* File created by MIDL compiler version 8.00.0595 */
+/* at Tue Dec 25 23:32:30 2018
  */
 /* Compiler settings for MEXSample.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0595 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
